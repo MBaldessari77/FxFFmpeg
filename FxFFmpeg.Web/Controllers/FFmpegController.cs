99cfb8ab8076550ab44ebe603871000da41a6817
Marco Baldessari <mb@sinformatic.com>
2017-12-04 00:40:42 +0100

    Add controllers and models to web app

---
+++ FxFFmpeg.Web/Controllers/FFmpegController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RxFFmpegCore.Models;
+using RxFFmpegWebCore.Model;
+
+namespace FxFFmpeg.Web.Controllers
+{
+	[Route("api/[controller]")]
+	public class FFmpegController : Controller
+	{
+		[HttpGet]
+		[Route("[action]")]
+		public async Task<IActionResult> GetMediaInfo(string path)
+		{
+			FFmpegTask ffmpeg = new FFmpegTask();
+			ffmpeg.Start($"-i \"{path}\"");
+
+			List<MediaVideoStreamView> outputs = new List<MediaVideoStreamView>();
+
+			while (true)
+			{
+				var output = await ffmpeg.GetOutputAsync();
+
+				if (output == null)
+					break;
+
+				var mediaVideoStream = output as MediaVideoStream;
+				if (mediaVideoStream != null)
+					outputs.Add(new MediaVideoStreamView(mediaVideoStream));
+			}
+
+			return Ok(outputs);
+		}
+	}
+}
---
+++ FxFFmpeg.Web/Controllers/MediaFilesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RxFFmpegCore.Services;
+
+namespace FxFFmpeg.Web.Controllers
+{
+	[Route("api/[controller]")]
+	public class MediaFilesController : Controller
+	{
+		readonly MediaFileService _mediaFileServices;
+
+		public MediaFilesController(MediaFileService mediaFileServices) { _mediaFileServices = mediaFileServices; }
+
+		[HttpGet]
+		[Route("[action]")]
+		public async Task<IActionResult> GetFiles(string path, bool includeSubFolders) { return Ok(await Task.Run(() => _mediaFileServices.GetMediaFiles(path, includeSubFolders).OrderByDescending(f => f.SizeInGb))); }
+	}
+}
