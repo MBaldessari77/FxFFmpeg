c38344ea0d865ca47bc2144e000a939ac7ccd3b3
Marco Baldessari <mb@sinformatic.com>
2018-03-31 17:47:40 +0200

    Fixed display of unknown encoding

--- FxFFmpeg.Web/Models/MediaVideoStreamView.cs
+++ FxFFmpeg.Web/Models/MediaVideoStreamView.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable InconsistentNaming
+using System;
 using System.Linq;
 using FxFFmpeg.Core;
 using FxFFmpeg.Models;
@@ -12,7 +15,6 @@ namespace FxFFmpeg.Web.Models
 		public MediaVideoStreamView(MediaVideoStream mediaVideoStream) { _mediaVideoStream = mediaVideoStream; }
 
 		public bool IsH264 => _mediaVideoStream.Attributes.Any(a => a.IndexOf(FFMpegConstants.H264, StringComparison.OrdinalIgnoreCase) != -1);
-		// ReSharper disable once InconsistentNaming
 		public bool IsHEVC => _mediaVideoStream.Attributes.Any(a => a.IndexOf(FFMpegConstants.HEVC, StringComparison.OrdinalIgnoreCase) != -1);
 		public bool IsUnknown => !IsH264 && !IsHEVC;
 		public string VideoStreamTypeRaw => _mediaVideoStream.Attributes.FirstOrDefault() ?? "N/A";
