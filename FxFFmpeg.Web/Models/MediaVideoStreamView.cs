4b8ff625659dbda3d638ce56567d225322513c06
Marco Baldessari <mb@sinformatic.com>
2018-04-01 12:25:47 +0200

    Add information about resolutions

--- FxFFmpeg.Web/Models/MediaVideoStreamView.cs
+++ FxFFmpeg.Web/Models/MediaVideoStreamView.cs
@@ -3,6 +3,7 @@
 // ReSharper disable InconsistentNaming
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using FxFFmpeg.Core;
 using FxFFmpeg.Models;
 
@@ -18,5 +19,41 @@ namespace FxFFmpeg.Web.Models
 		public bool IsHEVC => _mediaVideoStream.Attributes.Any(a => a.IndexOf(FFMpegConstants.HEVC, StringComparison.OrdinalIgnoreCase) != -1);
 		public bool IsUnknown => !IsH264 && !IsHEVC;
 		public string VideoStreamTypeRaw => _mediaVideoStream.Attributes.FirstOrDefault() ?? "N/A";
+
+		public string Resolution => ProcessResolution();
+
+		string ProcessResolution()
+		{
+			const string pattern = @"^\d+x\d+";
+        
+			foreach (string attribute in _mediaVideoStream.Attributes)
+			{
+				Match m = Regex.Match(attribute, pattern);
+				
+				if(!m.Success)
+					continue;
+
+				string[] resolutions = m.Value.Split('x');
+
+				int x = int.Parse(resolutions[0]);
+				int y = int.Parse(resolutions[1]);
+
+				if (y == 720)
+					return "HD";
+
+				if (y == 1080)
+					return "FULL HD";
+
+				if (x == 2560)
+					return "2K";
+
+				if (x == 3840)
+					return "4K UHD";
+
+				return m.Value;
+			}
+
+			return "N/A";
+		}
 	}
 }
